4b34b4461a83d7299ee7eb7ed649f4ae5c040051
j3br <jens@myrk.se>
2023-05-17 23:09:41 +0200

    add random footer msg

--- src/config/components/footer.js
+++ src/config/components/footer.js
@@ -1,7 +1,14 @@
 import { getHomePermalink, getPermalink } from "~/utils/permalinks";
 import { currentYear } from "~/utils/utils";
 
+const poweredByMessages = [
+    "Powered by twisted logic",
+    "Powered by irony and despair",
+    "Powered by quantum entanglement",
+];
+const randomMessage = poweredByMessages[Math.floor(Math.random() * poweredByMessages.length)];
+
 const footerData = {
-    footNote: '© ' + currentYear
+    footNote: '© ' + currentYear + ' · ' + randomMessage
 };
 export default footerData;
