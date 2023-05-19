98ebc8151515f8603b3819c2aa36e77df0f154d5
j3br <jens@myrk.se>
2023-05-19 23:00:51 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import UnoCSS from "unocss/astro";
 
 import SITE from "./src/config/site/config.mjs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: SITE.site,
@@ -17,4 +22,16 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
+  vite: {
+    optimizeDeps: {
+      exclude: [
+        'limax',
+      ],
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
