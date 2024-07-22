43db27b2c0cf9d4309913070221d0f1b34c59c41
j3br <11167461+j3br@users.noreply.github.com>
2024-07-22 19:13:15 +0200

    chore: remove unused import

--- src/config/components/footer.js
+++ src/config/components/footer.js
@@ -1,14 +1,13 @@
-import { getHomePermalink, getPermalink } from "~/utils/permalinks";
 import { currentYear } from "~/utils/utils";
 
 const poweredByMessages = [
-    "Powered by twisted logic",
-    "Powered by irony and despair",
-    "Powered by quantum entanglement",
+  "Powered by twisted logic",
+  "Powered by irony and despair",
+  "Powered by quantum entanglement",
 ];
 const randomMessage = poweredByMessages[Math.floor(Math.random() * poweredByMessages.length)];
 
 const footerData = {
-    footNote: '© ' + currentYear + ' · ' + randomMessage
+  footNote: '© ' + currentYear + ' · ' + randomMessage
 };
 export default footerData;
